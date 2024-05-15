2983292ac64087652c271d521b7f2bbe39fc5fd4
Abdulloh Xo'jayev <xodjaevabdulloh54@gmail.com>
2024-05-15 11:34:21 +0500

    tugadi

---
+++ index.js
@@ -0,0 +1,11 @@
+let ism = prompt("Ismingizni kiriting");
+let id = prompt("Space ID ni kiriting");
+let password = prompt("Space password ni kiriting");
+
+let data = {
+    ism: ism,
+    id: id,
+    password: password,
+}
+
+console.log(data);
